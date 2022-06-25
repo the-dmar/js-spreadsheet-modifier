2cd228a79ab04d6ae1f824237964adf03b2652ac
dmm22 <davidmartin@dgaauto.com>
2022-06-25 17:00:37 -0400

    Create function to parse and reformat invalid json

--- src/convertJsonToCsv.ts
+++ src/convertJsonToCsv.ts
@@ -1,6 +1,31 @@
+interface Test {
+  [key: string]: any
+}
+
 const convertJsonToCsv = (e: Event) => {
-  const pastedJson = (e.target as HTMLTextAreaElement).value
-  console.log(pastedJson)
+  const pastedValue = (e.target as HTMLTextAreaElement).value
+  if (pastedValue) {
+    try {
+      const json = JSON.parse(pastedValue)
+
+      const result = Array.isArray(json)
+        ? json.map((item: Test) => reformatNestedValues(item))
+        : reformatNestedValues(json)
+      console.log(result)
+    } catch {
+      alert("Invalid JSON!")
+    }
+  }
+}
+
+const reformatNestedValues = (jsonObject: Test) => {
+  Object.keys(jsonObject).forEach(key => {
+    if (typeof jsonObject[key] === "object") {
+      jsonObject = { ...jsonObject, [key]: JSON.stringify(jsonObject[key]) }
+    } else return jsonObject[key]
+  })
+
+  return jsonObject
 }
 
 export default convertJsonToCsv
