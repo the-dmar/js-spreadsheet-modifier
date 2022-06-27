9ad0863b278f5bdb7606d17ef53a8d6ce2f4d317
dmm22 <davidmartin71090@gmail.com>
2022-06-27 18:13:21 -0400

    Rename a few functions, and the Test interface
    Test was supposed to be temporary, renamed that to UnknownObject
    
    In the interest of each function doing a single task, I moved the logic for validating json into checkJsonFormat

--- src/convertJsonToCsv.ts
+++ src/convertJsonToCsv.ts
@@ -1,24 +1,29 @@
-interface Test {
+interface UnknownObject {
   [key: string]: any
 }
 
 const convertJsonToCsv = (e: Event) => {
   const pastedValue = (e.target as HTMLTextAreaElement).value
+  const jsonWithNestedValuesAsStrings =
+    pastedValue && checkJsonFormat(pastedValue)
+}
+
+const checkJsonFormat = (pastedValue: string) => {
   if (pastedValue) {
     try {
       const json = JSON.parse(pastedValue)
 
       const result = Array.isArray(json)
-        ? json.map((item: Test) => reformatNestedValues(item))
+        ? json.map((item: UnknownObject) => reformatNestedValues(item))
         : reformatNestedValues(json)
       console.log(result)
     } catch {
-      alert("Invalid JSON!")
+      return null
     }
   }
 }
 
-const reformatNestedValues = (jsonObject: Test) => {
+const reformatNestedValues = (jsonObject: UnknownObject) => {
   Object.keys(jsonObject).forEach(key => {
     if (typeof jsonObject[key] === "object") {
       jsonObject = { ...jsonObject, [key]: JSON.stringify(jsonObject[key]) }
