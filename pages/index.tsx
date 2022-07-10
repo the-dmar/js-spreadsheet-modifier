406adef6e8c188c3e5a88f0434c54986a95e4d13
dmm22 <davidmartin71090@gmail.com>
2022-07-10 11:14:08 -0400

    Add button to load files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,20 @@
+import { useRef } from "react"
+import readXlsxFile from "read-excel-file"
+
 const Home = () => {
+  const uploadRef = useRef<HTMLInputElement | null>(null)
+
+  const uploadHandler = async (files: FileList | null) => {
+    const uploadedFile = files && files[0]
+    console.log(uploadedFile)
+  }
+
   return (
-    <div>
-      <p>Home</p>
-    </div>
+    <input
+      type="file"
+      onChange={e => uploadHandler(e.target.files)}
+      ref={uploadRef}
+    />
   )
 }
 
